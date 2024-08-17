705ec3526e58365b34896a385749025768266248
pi big star <103993642+qcy1024@users.noreply.github.com>
2024-08-17 22:45:31 +0800

    Delete build directory

--- build/CMakeFiles/requirement.dir/app/global.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/requirement.dir/app/global.cpp.o: \
- /mnt/hgfs/traffic_analyze_100G/requirement\ copy/app/global.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/requirement.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for requirement.
-# This may be replaced when dependencies are built.
